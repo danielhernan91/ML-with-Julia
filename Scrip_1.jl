e6dc15a1096e43ad1cd2d5d06412bd1d1c4f09f4
Daniel Hernan Freire <160794143+danielhernan91@users.noreply.github.com>
2025-07-20 20:04:29 -0500

    Update Scrip_1.jl

--- Scrip_1.jl
+++ Scrip_1.jl
@@ -1,4 +1,28 @@
 
 # Install Pkg.add("MLDatasets") to use data
+using Pkg
+#Pkg.add("MLDatasets")
+using MLDatasets
 
-Pkg.add("MLDatasets")
+using MLJ
+
+# small mincer data
+import DataFrames: DataFrame
+X = DataFrame(
+    name = ["Siri", "Robo", "Alexa", "Cortana"],
+    gender = ["male", "male", "Female", "female"],
+    likes_soup = [true, false, false, true],
+    height = [152, missing, 148, 163],
+    rating = [2, 5, 2, 1],
+    outcome = ["rejected", "accepted", "accepted", "rejected"],
+)
+schema(X)
+# working with categorical data
+X.outcome = coerce(X.outcome, OrderedFactor)
+levels(X.outcome)
+
+Xnew = coerce(X, :gender     => Multiclass,
+                 :likes_soup => OrderedFactor,
+                 :height     => Continuous,
+                 :rating     => OrderedFactor)
+schema(Xnew)
